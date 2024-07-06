c54dc08386ed95632d0a55ae154b4d6b29850997
Kawan Amelung <kawan.amelung@gmail.com>
2024-07-06 20:31:48 +0200

    move to styles.css

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,23 @@
-var map = L.map('map').setView([0, 0], 2); // Centered at [0, 0] with zoom level 2
+var map = L.map('map', {
+    center: [0, 0],
+    zoom: 2,
+    zoomAnimation: true,
+    zoomAnimationThreshold: 4,
+    fadeAnimation: true,
+    easeLinearity: 0.25,
+    zoomSnap: 0.1,
+    zoomDelta: 0.5,
+    wheelPxPerZoomLevel: 60
+});
 
 // Add Mapbox Satellite tiles using the environment variable
 L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/{z}/{x}/{y}?access_token=' + mapboxAccessToken, {
     attribution: '© Mapbox © OpenStreetMap contributors',
-    tileSize: 512,
-    zoomOffset: -1
+    tileSize: 256, // Change tile size to 256
+    zoomOffset: 0, // Adjust zoom offset for the new tile size
+    updateWhenIdle: true,
+    updateWhenZooming: false,
+    maxNativeZoom: 19, // Adjust based on the level of detail needed
 }).addTo(map);
 
 // Load volcano data from the API
@@ -14,9 +27,9 @@ fetch('/api/volcanoes')
         data.forEach(volcano => {
             var marker = L.marker([volcano.latitude, volcano.longitude]).addTo(map);
             marker.bindTooltip(volcano.name, {
-                permanent: false,  // Tooltip is shown on hover
-                direction: 'top',  // Tooltip appears above the marker
-                offset: [0, -10],  // Adjust tooltip position if necessary
+                permanent: false,
+                direction: 'top',
+                offset: [0, -10],
             });
             marker.on('click', function() {
                 window.location.href = `/volcano/${volcano.id}`;
@@ -26,14 +39,3 @@ fetch('/api/volcanoes')
     .catch(error => {
         console.error('Error fetching volcano data:', error);
     });
-
-// // Add logo to the map
-// var logo = L.control({ position: 'topright' });
-//
-// logo.onAdd = function(map) {
-//     var div = L.DomUtil.create('div', 'my-logo');
-//     div.innerHTML = '<img src="/static/img/logo.png" alt="Logo" style="width: 100px; height: auto;">';
-//     return div;
-// };
-//
-// logo.addTo(map);
